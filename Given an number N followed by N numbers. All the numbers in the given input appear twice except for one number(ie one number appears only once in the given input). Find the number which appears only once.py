09b44434d68add6b1a6c079cf6a47426038a5cfc
rarnjithkumar <53113753+rarnjithkumar@users.noreply.github.com>
2019-07-27 11:22:58 -0800

    Create Given an number N followed by N numbers. All the numbers in the given input appear twice except for one number(ie one number appears only once in the given input). Find the number which appears only once.py

---
+++ Given an number N followed by N numbers. All the numbers in the given input appear twice except for one number(ie one number appears only once in the given input). Find the number which appears only once.py
@@ -0,0 +1,10 @@
+r=int(input())
+test_list=list(map(int,input().strip().split()))[:r]
+c=0
+for i in range(len(test_list)):
+  if (i==test_list[i]):
+    print(test_list[i],end=" ")
+  else:
+    c=c+1
+  if c==(len(test_list)):
+    print('-1')
