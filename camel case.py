0546e1fe80d224d30f01380d10e263f11535de2b
rarnjithkumar <53113753+rarnjithkumar@users.noreply.github.com>
2019-07-29 16:00:46 +0530

    Update camel case.py

--- camel case.py
+++ camel case.py
@@ -1,17 +1,3 @@
-def convert(r): 
-    if(len(r)==0): 
-        return
-    r1='' 
-    r1+=r[0].upper() 
-    for i in range(1,len(r)- 1): 
-        if(r[i] ==' '): 
-            r1+=r[i+1].upper() 
-            i+=1
-        elif(r[i - 1]!=' '): 
-            r1+=r[i]  
-    print(r1)      
-def main(): 
-    r=input()
-    convert(r)   
-if __name__=="__main__": 
-    main()
+r=input()
+r.title()
+print(r.title())
